755300d759496b1fed1b2ce80db9fd1fb5b852f3
AeDudics <142440963+AeDudics@users.noreply.github.com>
2024-01-23 00:16:15 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,14 +1,24 @@
-
-
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
+		for(isPrime == false){
+			Double.parseDouble(e)
+		}
 		System.out.println("Replace this string with your answer!");
 	}
 	
 
 	//Finish this function
 	public boolean isPrime(double d){
+		  if(d < 2.0){
+    		  	return false;
+  		}
+  		  for(double i = 2.0; i <= Math.sqrt(d); i++){
+    		  	if(d%i == 0){
+      				return false;
+    			}
+  		}
+  	return true;
 	}
 
 }
